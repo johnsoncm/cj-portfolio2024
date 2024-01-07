f3e0d0081c4331c72c14e2c9e04afe86c407aec5
Casey Johnson <johnsoncm3@gmail.com>
2024-01-07 16:02:23 -0500

    initial commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,7 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Casey Johnson - Software Engineer" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
@@ -10,7 +10,8 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Casey Johnson</h1>
+      <h2>Software Engineer</h2>
       <ul>
         <li>
           <a
